79e99c870b6e5c767c12aaba94830fd520ee9e81
dyooreen <dyooreen@icloud.com>
2024-12-02 21:42:58 +0400

    .

--- lib/components/ThemeBuilder.tsx
+++ lib/components/ThemeBuilder.tsx
@@ -2,7 +2,6 @@ import { ClassesProvider } from "../../src/Context/ClassesContext"
 import { PopoverProvider } from "../../src/Context/PopoverContext"
 import Popover from "./Popover"
 import TogglePopupButton from "./TogglePopoverButton"
-import "../index.css"
 
 const ThemeBuilder = () => {
 
