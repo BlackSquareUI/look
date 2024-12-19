e81eebc1f28a1c6705d654993a7cf68c76e9ea9b
dyooreen <dyooreen@icloud.com>
2024-12-19 22:45:51 +0400

    update to static class

--- src/components/Popover.tsx
+++ src/components/Popover.tsx
@@ -6,7 +6,7 @@ const Popover = () => {
     const { isHidden } = usePopoverContext()
     return (
         isHidden &&
-        <div className={"oo-background-color-primary oo-border-color oo-border-radius ee-border-style_solid ee-width_22 oo-padding ee-position_fixed ee-bottom_6 ee-right_2 oo-border-width"}>
+        <div className="oo-background-color-primary oo-border-color oo-border-radius ee-border-style_solid ee-width_22 oo-padding ee-position_fixed ee-bottom_6 ee-right_2 oo-border-width">
             <div className="ee-background-color_white ee-height_auto ee-overflow_auto ee-height_18 oo-border-color oo-border-width ee-border-style_solid oo-border-radius">
                 <Props />
             </div>
